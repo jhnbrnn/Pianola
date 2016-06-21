607b5b4e747b6ee156c3ef8070dc9c12166d6715
John Brennan <john@jhnbrnn.com>
2016-06-21 08:28:57 -0400

    Add gulp and webpack files

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+  entry: './src/App.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: 'public/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+  devtool: 'source-map'
+}
